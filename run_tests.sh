f707970cd1617f7a547f035e6edb05c756e5cfc1
erseco <erseco@gmail.com>
2023-10-02 21:52:37 +0100

    Fixed test

--- run_tests.sh
+++ run_tests.sh
@@ -12,4 +12,4 @@ echo "moodle is ready"
 # Give it another 3 seconds.
 sleep 3;
 
-curl --silent --fail http://app:8080 | grep '<title>Dockerized_Moodle</title>'
+curl --silent --fail http://app:8080 | grep '<title>Home | moodle</title>'
